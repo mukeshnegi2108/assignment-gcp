4e61542276493a727a44fb5f82ef7a35b9d70cb2
mukeshnegi2108 <negimukesh21@gmail.com>
2023-06-05 21:51:42 +0530

    code change final

--- jenkins/library/buildAndPushDockerImage.groovy
+++ jenkins/library/buildAndPushDockerImage.groovy
@@ -1,7 +1,20 @@
-def call(String dockerfilePath, String imageName, String gcrRegistry) {
+def buildDockerImage(String dockerfilePath, String imageName, String gcrRegistry) {
     stage("Build Docker Image") {
+        // Retrieve Google Cloud credentials
+        withCredentials([googleServiceAccount(credentialsId: 'GoogleAuthenticator')]) {
+             def googleCredentialsKeyfileJson = credentials('GoogleAuthenticator')
+
+            sh "gcloud auth activate-service-account --key-file=${googleCredentialsKeyfileJson}"
+            sh "gcloud config set project ${googleProjectId}"
+        }
+
+        // Build Docker image
         sh "docker build -t ${imageName} -f ${dockerfilePath} ."
+        
+        // Tag the image with the GCR registry URL
         sh "docker tag ${imageName} ${gcrRegistry}/${imageName}"
+        
+        // Push the image to GCR
         sh "docker push ${gcrRegistry}/${imageName}"
     }
 }
